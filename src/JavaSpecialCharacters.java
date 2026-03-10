08803d34c8eeeca7e57a4a92bc9140132ea95ce8
Jedidiah Owusu-Ansah Manu <manujedidiah95@gmail.com>
2026-03-10 17:21:55 +0000

    Special String Characters

---
+++ src/JavaSpecialCharacters.java
@@ -0,0 +1,15 @@
+public class JavaSpecialCharacters {
+    public static void main(String[] args) {
+        // Strings -Special Characters
+
+        //(\""), this is used for quotation
+        String txt = "We are the so-called \"Vikings\" from the north.";
+
+        //(\'), this is for inserting a single quote
+
+        String ung = "It\'s alright.";
+
+        System.out.println(txt);
+        System.out.println(ung);
+    }
+}
